19db68f237813833ff5d2d346edbb9b7cfcc4e09
Abin Augustine <abin.augustine@cctsglobal.com>
2025-07-04 12:28:35 +0530

    Added contact navigation

--- Frontend/src/components/Footer.js
+++ Frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer" id="contact">
       <div className="container">
         <div className="footer-content">
           <div className="footer-section">
